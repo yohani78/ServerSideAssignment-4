3b6799a7efa7de7933b8b66bf55bcbd8ccbf7d69
yohani78 <yohani78@yahoo.com>
2015-09-30 18:59:01 +0000

    Yohani78 pushing now

---
+++ serverAssigment-4.php
@@ -0,0 +1,81 @@
+<?php 
+                $name = "Yohani Gonzalez  3 JavaScript Sample Code Assignment";
+                
+            // Output sample 
+        
+                     
+                
+            // Statement sample 
+        
+                    
+
+            // variable samples
+            
+                     
+        
+            // Operators:
+            
+                    
+                
+            // Arithmetic:
+            
+                    
+            
+            // Assignment
+            
+                    
+                    
+            // Data Types
+                    
+                    
+                    
+            // Functions
+                    
+                    
+                    
+            // Objects
+                    
+                   
+                    
+            // Strings
+                    
+                    
+                    
+            // String Methods
+                    
+                    
+                    
+            // Numbers
+                    
+                    
+                    
+            // Number Methods
+                    
+                    
+                    
+            // Arrays
+                    
+                    
+                    
+            // Array Methods
+                    
+                    
+                    
+            // Booleans
+                    
+                    
+                    
+            // Comparisons
+                    
+                    
+                    
+
+
+
+
+
+
+
+
+
+?>
