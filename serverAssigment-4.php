9afd4645ca2437885dae78bee1c968bbb6579995
yohani78 <yohani78@yahoo.com>
2015-10-01 16:50:32 +0000

    yo

--- serverAssigment-4.php
+++ serverAssigment-4.php
@@ -1,7 +1,7 @@
 <?php 
                 $name = "Yohani Gonzalez  3 PHP Sample Code Assignment";
                 
-//  Variable Types:       
+------------ Variable Types:( Boolean, Integer, Float, String, Array, Object )       
  
 
     
@@ -11,17 +11,17 @@
             $response = true;
             $this->cache[$key] = false;
             
-    Integer   // an integer $variable is a (number 1, -2, 21e5, 2100000 [but not a decimal 0.2])
+    Integer   // an integer $variable is a (number 1, -2, 21e5, 2100000 [but not a decimal 0.2]) value
             
             
             
-    Float   // a float $variable is a (Decimal number 1.0, -0.02, 21e-5, 0.00021 )
+    Float   // a float $variable is a (Decimal number 1.0, -0.02, 21e-5, 0.00021 ) value
     
             $version = ($type == self::VERSION_TYPE_FLOAT ? $this->prepareVersionNo($match[1]) : $match[1]);
             $this->is('iOS') && $this->version('iPad', self::VERSION_TYPE_FLOAT) >= 4.3 
             $this->version('Windows Phone OS', self::VERSION_TYPE_FLOAT) >= 7.5 
             
-    String   // a
+    String   // a string $variable is a (words, letter, ) value
             
             if (strpos($header, '_') === false) {
             $header = str_replace('-', '_', $header);
@@ -32,7 +32,7 @@
             $_rules = array_change_key_case($this->getRules());
             
             
-    Array   // a
+    Array   // an array $variable is a (group of key & value combination) object
     
             array $headers = null,
             protected static $phoneDevices = array();
@@ -45,7 +45,7 @@
 
             
             
-    Object   // a
+    Object   // an object $variable is a 
     
             if (!$rules) {
                             $rules = array_merge(
@@ -59,7 +59,7 @@
 
             
             
-    Null   // a
+    Null   // a null $variable is a
     
             protected $userAgent = null;
             'HTTP_PROFILE' => null,
